2cc505ccc5306e5641f506909986ba9cb19bfdff
aliokurresmi <143112456+aliokurresmi@users.noreply.github.com>
2023-08-25 19:55:41 +0300

    Add files via upload

---
+++ project-1.py
@@ -0,0 +1,25 @@
+print("this program adds the digits of the numbers you enter!")
+
+sayi = input("enter the number : \n")
+
+toplam = 0 
+
+for i in sayi:
+    toplam += int(i)
+
+print(sayi + " sum of digits of the number = ",toplam)    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
